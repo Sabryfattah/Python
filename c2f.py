3fd9665fe70eebe7192ea8b62e3e058217778942
Sabryfattah <sf3000@hotmail.com>
2020-03-22 13:06:51 +0000

    Update c2f.py

--- c2f.py
+++ c2f.py
@@ -1,15 +1,13 @@
+"""convert temperature from Celsius to Fahrenheit and vice versa."""
 import argparse
-
-parser = argparse.ArgumentParser(description = """
-This script convert temperature from Celsius to Fahrenheit and vice versa.
-e.g. c2f 100 f    #=> 38 Celsius.
-e.g. c2f 38 c #=> 100 Fahrenheit""",
-usage = "conv [degrees] [scale]"	)
-parser.add_argument("degrees",  help="degrees of temperature to convert.")
-parser.add_argument("scale",  help="type of temperature scale (f or c)")
+#--------------------------------------------------------------------------------------
+parser = argparse.ArgumentParser(description =__doc__,
+usage = "c2f <scale> [degree]")
+parser.add_argument("degree", action="store", help="temperature degree")
+parser.add_argument("-f", "--fahrenheit", action="store_true", help="fahenheit")
+parser.add_argument("-c", "--celsius", action= "store_true",  help="celsius")
 args = parser.parse_args()
-
-if args.scale == 'f':
-	print  (float(args.degrees)- 32) * 0.555556, " Celsius" 
-else:
-	print (float(args.degrees) * 1.8) + 32, " Fahrenheit"
+#---------------------------------------------------------------------------------------
+if __name__ == "__main__":
+	if args.fahrenheit : print(round(float(args.degree) - 32 * 0.55, 2), " Celsius")
+	if args.celsius : print(float(args.degree) * 1.8 + 32, " Fahrenheit")
