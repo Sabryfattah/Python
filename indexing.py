ba8fb514072a01b23789a3743e21a8c21456fd0a
Sabryfattah <sf3000@hotmail.com>
2020-05-30 15:57:53 +0100

    Delete indexing.py

--- indexing.py
+++
@@ -1,9 +0,0 @@
-import os
-from glob import glob
-import sys
-#========================================================
-folder = "D:\Documents"
-
-result = [y for x in os.walk(folder) for y in glob(os.path.join(x[0], "*.txt"))]
-for f in result:
-	open("index.txt", "a").write(f+"\n")
